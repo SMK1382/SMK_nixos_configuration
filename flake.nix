f9664d9b7d1a0d0a6054d6318d749d3507fc8983
mohammadreza_kalali <kalalimohammad1382@gmail.com>
2026-01-03 16:08:34 +0330

    config neovim

--- flake.nix
+++ flake.nix
@@ -2,24 +2,36 @@
   description = ":)";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixvim, ... }: {
+  outputs = { self, nixpkgs, home-manager, nixvim, ... }:
+  let
+    system = "x86_64-linux";
+  in {
     nixosConfigurations.SMK-HOST = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ 
+      modules = [
         ./configuration.nix
-	home-manager.nixosModules.home-manager
+        home-manager.nixosModules.home-manager
+          {
+          home-manager.users.mohammadreza = {
+            imports = [
+              nixvim.homeManagerModules.nixvim
+            ];
+          };
+        }
       ];
     };
   };
 }
+
